472c9241fc037eed6fba2fac481635b85a04b0a6
Maitrik Patel <maitrik.patel@docusign.com>
2016-09-29 10:00:55 -0700

    morning 29

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,43 +6,64 @@
 
 (function($) {
 
-	var signature = $('#signature').text();
-	// console.log(signature);
+	var username = $("input#username").val();
+	var password = $("input#password").val();
+  console.log(username);
+	console.log(password);
+
+	// $(function() {
+	// 	$.ajax({
+	// 		type: 'GET',
+	// 		contentType: 'application/json',
+	// 		accept: 'application/json',
+	// 		url: 'https://demo.docusign.net/restapi/v2/login_information',
+	// 		data : 'api_password=true&include_account_id_guid=true',
+	// 		// username: 'dsemailer.manual+alexlee@gmail.com',
+	//     // password: 'test1234',
+	// 		// IntegratorKey: '405d6f8d-d54e-41aa-b7fd-ab99a1db5bb7',
+	// 		headers: {
+	//     					"X-DocuSign-Authentication":{
+	// 									"Username":"dsemailer.manual+alexlee@gmail.com",
+	// 									"Password":"test1234",
+	// 									"IntegratorKey":"405d6f8d-d54e-41aa-b7fd-ab99a1db5bb7"
+	// 								}
+	// 							// 'Username': 'dsemailer.manual+alexlee@gmail.com',
+	// 							// 'Password': 'test1234',
+	// 							// 'IntegratorKey': '405d6f8d-d54e-41aa-b7fd-ab99a1db5bb7'
+	// 						},
+	// 		success: function(data, textStatus, jqXHR){alert('Stock updated successfully. Status: '+textStatus);},
+	// 		error: function(jqXHR, textStatus, errorThrown){alert('update Stock error: ' + textStatus);}
+	// 	});
+	// });
+
+	var envelope = $('#envelope').text()
+
+	var signature = $('#signature').text()
+
+	// for ( i = 0; i < envelope; i++){
+	// 	another get call
+	// 	signature = signature + each envelope[i] -> signers -> tabs -> signatureid
+	// 	pages = pages + each envelope[i] -> signers -> tabs -> signatureid -> pagenumbers
+	// }
 
 	var pages = Math.round((signature*5));
-	// console.log(pages);
 	$('#pages').text(pages);
 
 	var trees = Math.round((signature*0.6));
-	// console.log(trees);
 	$('#trees').text(trees);
-
 	var co2 = Math.round((signature*6.35169119*5) );
-	// console.log(co2);
 	$('#co2').text(co2);
-
 	var vex = Math.round(((signature*6.35169119*5)/13.6860625));
-	// console.log(vex);
 	$('#vex').text(vex);
-
 	var water = Math.round((signature*10*5));
-	// console.log(water);
 	$('#water').text(water);
-
 	var waste = Math.round((signature*2.2*5));
-	// console.log(waste);
 	$('#waste').text(waste);
-
 	var time = Math.round((signature*1.29));
-	// console.log(time);
 	$('#time').text(time);
-
 	var money = Math.round((signature*27.18));
-	// console.log(money);
 	$('#money').text(money);
-
 	var employees = Math.round( ((signature*27.18)/7.2)	);
-	// console.log(employees);
 	$('#employees').text(employees);
 
 	skel.breakpoints({
